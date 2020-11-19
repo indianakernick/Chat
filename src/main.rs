ef430bad6b8a46b974a08c79bbd2039ed5f7f793
Kerndog73 <kerndog73@gmail.com>
2020-11-19 16:44:47 +1030

    Askama example
    Hmm, server-side rendering... yeah, I think I'm gonna use Angular instead

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 use warp::Filter;
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
     let hello = warp::get()
-        .and(warp::path("hello"))
-        .and(warp::path::param())
-        .map(move |name: String| {
-            warp::reply::html(format!(r#"
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>Chat</title>
-                    </head>
-                    <body>
-                        <p style="font-family: Helvetica, sans-serif">Hello <b>{}</b></p>
-                    </body>
-                </html>
-                "#,
-                name
-            ))
+        .and(warp::path!("hello" / String))
+        .map(move |name| {
+            HelloTemplate {name}
         });
 
-    warp::serve(hello.with(warp::log("chat")))
+    // Can expand this to hello.or(something).or(something_else)
+    let routes = hello;
+
+    warp::serve(routes.with(warp::log("chat")))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }

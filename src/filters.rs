c5adec24d7e05311373c36456e66fcb505bdae11
Kerndog73 <kerndog73@gmail.com>
2020-11-24 10:55:31 +1030

    Use and_then instead of map

--- src/filters.rs
+++ src/filters.rs
@@ -4,30 +4,18 @@ use super::handlers;
 pub fn hello() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::get()
         .and(warp::path!("hello" / String))
-        .map(move |name| {
-            handlers::HelloTemplate::new(name)
-        })
+        .and_then(handlers::hello)
 }
 
 pub fn get_messages() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::get()
         .and(warp::path!("api" / "messages"))
-        .map(move || {
-            match handlers::read_database() {
-                Ok(val) => warp::reply::json(&val),
-                Err(e) => warp::reply::json(&format!("{}", e))
-            }
-        })
+        .and_then(handlers::get_messages)
 }
 
 pub fn post_message() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     warp::post()
         .and(warp::path!("api" / "post_message"))
         .and(warp::body::json())
-        .map(move |message: String| {
-            match handlers::append_to_database(message) {
-                Ok(_) => String::new(),
-                Err(e) => format!("{}", e)
-            }
-        })
+        .and_then(handlers::post_message)
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,31 +1,50 @@
 use std::fs;
 use std::io::Write;
 use askama::Template;
+use std::convert::Infallible;
 
 #[derive(Template)]
 #[template(path = "hello.html")]
-pub struct HelloTemplate {
+struct HelloTemplate {
     name: String,
 }
 
 impl HelloTemplate {
-    pub fn new(name: String) -> HelloTemplate {
+    fn new(name: String) -> HelloTemplate {
         HelloTemplate { name }
     }
 }
 
-pub fn read_database() -> std::io::Result<Vec<String>> {
+fn read_database() -> std::io::Result<Vec<String>> {
     Ok(fs::read_to_string("database.txt")?
         .lines()
         .map(String::from)
         .collect::<Vec<String>>())
 }
 
-pub fn append_to_database(item: String) -> std::io::Result<()> {
+fn append_to_database(item: String) -> std::io::Result<()> {
     fs::OpenOptions::new()
         .write(true)
         .append(true)
         .create(true)
         .open("database.txt")?
         .write_all((item + "\n").as_bytes())
+}
+
+pub async fn hello(name: String) -> Result<impl warp::Reply, Infallible> {
+    Ok(HelloTemplate::new(name))
+}
+
+pub async fn get_messages() -> Result<impl warp::Reply, Infallible> {
+    match read_database() {
+        Ok(val) => Ok(warp::reply::json(&val)),
+        Err(e) => Ok(warp::reply::json(&format!("{}", e)))
+    }
+}
+
+pub async fn post_message(message: String) -> Result<impl warp::Reply, Infallible> {
+    match append_to_database(message) {
+        Ok(_) => Ok(String::new()),
+        Err(e) => Ok(format!("{}", e))
+    }
 }

4e65828f1a64a2eea52b6c8b04aa2ef238106c93
Kerndog73 <kerndog73@gmail.com>
2020-12-12 12:00:38 +1030

    Channels are organized into groups
    Or should I say "groops". Thanks SQL for reserving all the useful english words

--- initialize.sql
+++ initialize.sql
@@ -1,33 +1,3 @@
-CREATE TABLE IF NOT EXISTS Message (
-    message_id SERIAL NOT NULL,
-    timestamp TIMESTAMPTZ NOT NULL,
-    author INTEGER,
-    content TEXT NOT NULL,
-    channel_id INTEGER NOT NULL,
-
-    PRIMARY KEY (message_id),
-
-    FOREIGN KEY (author)
-        REFERENCES Usr (user_id)
-        ON UPDATE CASCADE
-        ON DELETE SET NULL,
-
-    FOREIGN KEY (channel_id)
-        REFERENCES Channel (channel_id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
-);
-
-CREATE UNIQUE INDEX IF NOT EXISTS message_chan_mess_idx
-    ON Message (channel_id, message_id);
-
-CREATE TABLE IF NOT EXISTS Channel (
-    channel_id SERIAL NOT NULL,
-    name TEXT NOT NULL,
-
-    PRIMARY KEY (channel_id)
-);
-
 CREATE TABLE IF NOT EXISTS Usr (
     user_id SERIAL NOT NULL,
     name TEXT NOT NULL,
@@ -43,13 +13,66 @@ CREATE TABLE IF NOT EXISTS Session (
     session_id CHAR(16) COLLATE "C" NOT NULL,
     creation_time TIMESTAMPTZ NOT NULL,
     user_id INTEGER NOT NULL,
-    -- TODO: Maybe add an active_time to invalidate the session after no activity
     -- TODO: Maybe tie some client info to the session
 
     PRIMARY KEY (session_id),
 
     FOREIGN KEY (user_id)
         REFERENCES Usr (user_id)
-        ON UPDATE CASCADE
+        ON UPDATE NO ACTION
         ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS Groop (
+    group_id SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    picture TEXT,
+
+    PRIMARY KEY (group_id),
+
+    UNIQUE (name)
+);
+
+CREATE TABLE IF NOT EXISTS Channel (
+    channel_id SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    group_id INTEGER NOT NULL,
+
+    PRIMARY KEY (channel_id),
+
+    FOREIGN KEY (group_id)
+        REFERENCES Groop (group_id)
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS groop_channel_idx
+    ON Channel (group_id, channel_id);
+
+CREATE TABLE IF NOT EXISTS Message (
+    message_id SERIAL NOT NULL,
+    timestamp TIMESTAMPTZ NOT NULL,
+    author INTEGER,
+    content TEXT NOT NULL,
+    channel_id INTEGER NOT NULL,
+
+    PRIMARY KEY (message_id),
+
+    -- Not sure if simply anonymizing the messages of deleted accounts is really
+    -- the right thing to do. There's a whole lot of legal stuff that I could
+    -- dig into about this.
+    -- https://www.reddit.com/r/discordapp/comments/d90yzq/a_small_rant_on_privacy_issues_disregarding_of/?utm_source=share&utm_medium=web2x&context=3
+    -- I think I'll just copy Discord!
+    FOREIGN KEY (author)
+        REFERENCES Usr (user_id)
+        ON UPDATE NO ACTION
+        ON DELETE SET NULL,
+
+    FOREIGN KEY (channel_id)
+        REFERENCES Channel (channel_id)
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS channel_message_idx
+    ON Message (channel_id, message_id);

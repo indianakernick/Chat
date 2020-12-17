62479755a08b38ac46869b87615fff4e5e2e2f85
Kerndog73 <kerndog73@gmail.com>
2020-12-17 15:25:37 +1030

    Add function for validating channel names

--- src/database/channel.rs
+++ src/database/channel.rs
@@ -32,6 +32,37 @@ pub async fn valid_channel(pool: Pool, channel_id: ChannelID) -> Result<bool, Er
 }
 */
 
+const MAX_CHANNEL_NAME_LENGTH: usize = 32;
+
+pub fn valid_channel_name(name: &String) -> bool {
+    // A byte limit instead of a character limit is tempting...
+
+    if name.is_empty() {
+        return false;
+    }
+
+    let mut count = 0;
+
+    for ch in name.chars() {
+        count += 1;
+        if count > MAX_CHANNEL_NAME_LENGTH {
+            return false;
+        }
+        // Not 100% sure about this.
+        // I'm certain that whitespace shouldn't be allowed.
+        // This handles non-ascii alphanumeric characters but that might be a
+        // little tricky to mirror on the JavaScript end.
+        if !ch.is_alphanumeric() && ch != '-' && ch != '_' {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/// Assumes that the group_id is valid (because verifying it would require an
+/// additional query). Also assumes that the name is valid because it can be
+/// checked with the above function.
 pub async fn create_channel(pool: Pool, group_id: GroupID, name: &String)
     -> Result<ChannelID, PoolError> {
     // TODO: Channel name should be unique within the group

110248d5d90bb7751b9fddfb34353ebb71dbfa1e
Kerndog73 <kerndog73@gmail.com>
2020-12-27 15:45:59 +1030

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,8 +2,6 @@ fn main() {
     // This is so sad ;-(
     // https://github.com/rust-lang/cargo/issues/2599
 
-    println!("cargo:rerun-if-changed=client/vue.config.js");
-
     println!("cargo:rerun-if-changed=client/public/channel.html");
     println!("cargo:rerun-if-changed=client/public/favicon.ico");
     println!("cargo:rerun-if-changed=client/public/login.html");
@@ -11,17 +9,21 @@ fn main() {
     println!("cargo:rerun-if-changed=client/src/assets/anonymous.png");
 
     println!("cargo:rerun-if-changed=client/src/components/Channel.vue");
-    println!("cargo:rerun-if-changed=client/src/components/ChannelCreate.vue");
-    println!("cargo:rerun-if-changed=client/src/components/ChannelList.vue");
-    println!("cargo:rerun-if-changed=client/src/components/GroupTitle.vue");
+    println!("cargo:rerun-if-changed=client/src/components/ChannelCreateDialog.vue");
+    println!("cargo:rerun-if-changed=client/src/components/ChannelDeleteDialog.vue");
+    println!("cargo:rerun-if-changed=client/src/components/Group.vue");
+    println!("cargo:rerun-if-changed=client/src/components/GroupCreateDialog.vue");
     println!("cargo:rerun-if-changed=client/src/components/Message.vue");
     println!("cargo:rerun-if-changed=client/src/components/MessageList.vue");
     println!("cargo:rerun-if-changed=client/src/components/MessageSender.vue");
+    println!("cargo:rerun-if-changed=client/src/components/ModalDialog.vue");
     println!("cargo:rerun-if-changed=client/src/components/ProfileNav.vue");
     println!("cargo:rerun-if-changed=client/src/components/StatusMessage.vue");
 
     println!("cargo:rerun-if-changed=client/src/pages/channel/App.vue");
     println!("cargo:rerun-if-changed=client/src/pages/channel/main.js");
+
+    println!("cargo:rerun-if-changed=client/vue.config.js");
 
     let build = match std::env::var("PROFILE").unwrap().as_str() {
         "debug" => "build-dev",

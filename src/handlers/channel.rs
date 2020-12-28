eb5b6394e77df01dd151a4c4a0e2ee1f60ad5415
Kerndog73 <kerndog73@gmail.com>
2020-12-29 10:00:51 +1030

    Handle invalid group IDs
    Trying to access an invalid group takes you to the first group

--- src/handlers/channel.rs
+++ src/handlers/channel.rs
@@ -20,8 +20,7 @@ fn ser_json<T: Serialize>(value: &T) -> String {
     serde_json::to_string(value).unwrap().replace("</script>", "<\\/script>")
 }
 
-// TODO: Handle invalid group_id
-pub async fn channel(group_id: db::GroupID, mut channel_id: db::ChannelID, session_id: db::SessionID, pool: Pool)
+pub async fn channel(mut group_id: db::GroupID, mut channel_id: db::ChannelID, session_id: db::SessionID, pool: Pool)
     -> Result<Box<dyn warp::Reply>, warp::Rejection> {
     let user = match db::session_user(pool.clone(), &session_id).await? {
         Some(user) => user,
@@ -31,22 +30,27 @@ pub async fn channel(group_id: db::GroupID, mut channel_id: db::ChannelID, sessi
         )))
     };
 
-    let (group_list, channel_list) = futures::future::join(
-        db::group_list(pool.clone(), user.user_id),
-        db::group_channels(pool.clone(), group_id)
-    ).await;
+    let group_list = db::group_list(pool.clone(), user.user_id).await?;
 
-    let group_list = group_list?;
-    let channel_list = channel_list?;
-
-    if group_list.is_empty() || channel_list.is_empty() {
+    if group_list.is_empty() {
         return Ok(Box::new(warp::http::StatusCode::NOT_FOUND));
     }
 
     let group_name = match group_list.iter().find(|g| g.group_id == group_id) {
         Some(group) => group.name.clone(),
-        None => return Ok(Box::new(warp::http::StatusCode::NOT_FOUND))
+        None => {
+            group_id = group_list[0].group_id;
+            channel_id = 0;
+            group_list[0].name.clone()
+        }
     };
+
+    let channel_list = db::group_channels(pool.clone(), group_id).await?;
+
+    if channel_list.is_empty() {
+        return Ok(Box::new(warp::http::StatusCode::NOT_FOUND));
+    }
+
     let channel_name = match channel_list.iter().find(|c| c.channel_id == channel_id) {
         Some(channel) => channel.name.as_str(),
         None => {

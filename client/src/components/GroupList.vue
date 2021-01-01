476734f30db7c752b54c56de34ac4d94c042576b
Kerndog73 <kerndog73@gmail.com>
2021-01-01 13:04:05 +1030

    Group list color bleed fix
    For real this time! The last fix didn't work when the border radius changed. This new fix should work under all circumstances.

--- client/src/components/GroupList.vue
+++ client/src/components/GroupList.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="group-list-container scrollable-container">
     <div class="group-list-block scrollable-block">
-      <img
-        v-for="group in groupList"
-        class="group-list-item"
-        :class="{'active': group.group_id === currentGroupId}"
-        @click="$emit('selectGroup', group.group_id)"
-        :src="group.picture"
-        :title="group.name"
-        alt="Group picture"
-        width="64"
-        height="64"
-        referrerpolicy="no-referrer"
-      />
+      <div class="group-list-item-wrapper" v-for="group in groupList">
+        <img
+          class="group-list-item"
+          :class="{'active': group.group_id === currentGroupId}"
+          @click="$emit('selectGroup', group.group_id)"
+          :src="group.picture"
+          :title="group.name"
+          alt="Group picture"
+          width="64"
+          height="64"
+          referrerpolicy="no-referrer"
+        />
+        <div class="group-list-item-back"></div>
+      </div>
       <div
         class="group-list-create"
         @click="$emit('createGroup')"
@@ -56,24 +58,40 @@ $image-size: 64px;
 
 .group-list-item:hover, .group-list-item.active {
   border-radius: $image-size / 4;
-  background-color: $group-item-back;
+  + .group-list-item-back {
+    border-radius: $image-size / 4 - 1;
+  }
 }
 
 .group-list-create:hover {
   border-radius: $image-size / 4;
 }
 
-.group-list-item, .group-list-create {
+.group-list-item, .group-list-create, .group-list-item-back {
   border-radius: $image-size / 2;
   cursor: pointer;
-  transition: border-radius 0.2s ease, background-color 0.2s ease;
+  transition: border-radius 0.2s ease;
 }
 
 .group-list-item {
+  position: relative;
+  z-index: 1;
+}
+
+.group-list-item-back {
+  background-color: $group-item-back;
+  position: absolute;
+  width: $image-size - 2;
+  height: $image-size - 2;
+  left: 1px;
+  top: 1px;
+  border-radius: $image-size / 2 - 1;
+}
+
+.group-list-item-wrapper {
+  flex: 0 0 $image-size;
+  position: relative;
   margin: $padding $padding 0 $padding;
-  // TODO: This sort of breaks down when we change the border radius...
-  background-image: radial-gradient($group-item-back $image-size / 2 - 1, transparent $image-size / 2);
-  flex: 0 0 64px;
 }
 
 .group-list-create {
